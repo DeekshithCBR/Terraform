5fa88c53485560ea28c23a867deda46685aa5065
DeekshithCBR <121077300+DeekshithCBR@users.noreply.github.com>
2022-12-20 03:44:25 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -160,16 +160,16 @@ resource "aws_autoscaling_group" "DeekshithASG" {
     create_before_destroy = true
   }
  resource "aws_instance" "Ansible" {
- ami = "ami-0b0dcb5067f052a63"
- availability_zone = "us-east-1a"
- instance_type = "t2.micro"
- key_name = "FinalProject"
- subnet_id = "${aws_subnet.subnet1-public.id}"
- vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
- associate_public_ip_address = true	
- tags = {
-    Name = "Ansible-Server"
-     }
+      ami = "ami-0b0dcb5067f052a63"
+      availability_zone = "us-east-1a"
+      instance_type = "t2.micro"
+      key_name = "FinalProject"
+      subnet_id = "${aws_subnet.subnet1-public.id}"
+      vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+      associate_public_ip_address = true	
+      tags = {
+           Name = "Ansible-Server"
+            }
  }
 }
 
